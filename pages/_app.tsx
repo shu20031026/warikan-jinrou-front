0d2840b85f66f02c27574b4397f616f959f05013
shu20031026 <shu20031026@gmail.com>
2022-07-17 16:34:09 +0900

    fix: useEffectを使用

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import 'ress'
 import '~/styles/globals.scss'
 
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 import { RecoilRoot } from 'recoil'
 
 import { Authenticated } from '~/components/Authenticated'
@@ -9,16 +10,17 @@ import { AuthProvider } from '~/contexts/AuthContext'
 import { useScrollTop } from '~/hooks/useScrollTop'
 import { DefaultLayout } from '~/layouts/Default'
 
-document.addEventListener(
-  'touchmove',
-  (e) => {
-    e.preventDefault()
-  },
-  { passive: false }
-)
-
 export default function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   useScrollTop()
+  useEffect(() => {
+    document.addEventListener(
+      'touchmove',
+      (e) => {
+        e.preventDefault()
+      },
+      { passive: false }
+    )
+  }, [])
 
   return (
     <RecoilRoot>
